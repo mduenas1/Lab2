d3cde7ae730fa140545d583ab29fda9fb9c552b9
Michael <duenas.mike10@yahoo.com>
2020-08-19 15:23:34 -0700

    added the main file

---
+++ Lab2.cpp
@@ -0,0 +1,16 @@
+//Michael Duenas &
+//Description: Hello World in C++
+//===========================================================
+
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+
+int main(){
+	string name1 = "Michael Duenas";
+	hello1Function(name1);	//calls hello world function
+	string name2 = "your name2";
+	hello2Function(name1);	//calls hello world function 2
+	return 0;
+}
